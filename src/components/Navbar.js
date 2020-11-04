f350080bae65b7a0a1da5bf236ec58f2c217fafa
Nehorowo <trianglek4nz@gmail.com>
2020-11-04 18:14:28 +0500

    add css modules

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import './Navbar.css' 
+
 
 const Navbar = () => {
     return (
         <nav className='nav'>
-        <div><a href='#s'>Profile</a></div>
-        <div><a href='#s'>Messages</a></div>
-        <div><a href='#s'>News</a></div>
-        <div><a href='#s'>Music</a></div>
-        <div><a href='#s'>Settings</a></div>
+        <div className='navItem'><a href='#s'>Profile</a></div>
+        <div className='navItem'><a href='#s'>Messages</a></div>
+        <div className='navItem'><a href='#s'>News</a></div>
+        <div className='navItem'><a href='#s'>Music</a></div>
+        <div className='navItem'><a href='#s'>Settings</a></div>
         </nav>
     )
 }
