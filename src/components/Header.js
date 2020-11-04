f92b1b426febb8dc892188998b4c90404bc0956b
Nehorowo <trianglek4nz@gmail.com>
2020-11-04 07:37:24 +0500

    "first commit on master // grid + header, nav,  profile components"

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png' className='logo' alt='Here is the logo'/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div className='crop'>
+        <img src='https://sun9-56.userapi.com/NfPoY1Rr_ZMhE6LRQ4OD95THzrG-CcEg-l5xDA/qYFQMBAZKew.jpg' alt='mainImage'/>
+        </div> 
+        <div>
+          desc
+        </div>
+        <div>
+          post
+          <div>
+            new post
+          </div>
+        </div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+      </div>
+    )
+}
+
+export default Profile
