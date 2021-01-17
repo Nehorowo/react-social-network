d70c3f3a1b31483f922894bd1d8bf72af1ccdfba
Nehorowo <trianglek4nz@gmail.com>
2021-01-17 18:03:49 +0500

    + exact route

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import cssObject from './Dialogs.module.css'
+import {NavLink} from "react-router-dom"
 
 const Dialogs = (props) => {
   return (
-    <div className={cssObject.content}>
-        <h1>Dialogs</h1>
+    <div className={cssObject.dialogs}>
+        <div className={cssObject.dialogsItems}>
+          <div className={cssObject.item + ' ' + cssObject.active}>
+            <NavLink to="/dialogs/1">Dimych</NavLink>
+          </div>
+          <div className={cssObject.item}>
+            <NavLink to="/dialogs/2">Sawka</NavLink>
+          </div>
+          <div className={cssObject.item}>
+            <NavLink to="/dialogs/3">Olga</NavLink>
+          </div>
+          <div className={cssObject.item}>
+            <NavLink to="/dialogs/4">Alex</NavLink>
+          </div>
+        </div>
+        <div className={cssObject.messages}>
+            <div className={cssObject.message}>message 1</div>
+            <div className={cssObject.message}>message 2</div>
+            <div className={cssObject.message}>message 3</div>
+        </div>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,8 +5,8 @@ import cssObject from './Navbar.module.css'
 const Navbar = () => {
     return (
         <nav className={cssObject.nav}>
-        <div className={cssObject.navItem}><NavLink to='/profile'>Profile</NavLink></div>
-        <div className={cssObject.navItem}><NavLink to='/dialogs'>Dialogs</NavLink></div>
+        <div className={cssObject.navItem}><NavLink to='/profile' activeClassName={cssObject.activeLink}>Profile</NavLink></div>
+        <div className={cssObject.navItem}><NavLink to='/dialogs' activeClassName={cssObject.activeLink}>Dialogs</NavLink></div>
         <div className={cssObject.navItem}><NavLink to='/news'>News</NavLink></div>
         <div className={cssObject.navItem}><NavLink to='/music'>Music</NavLink></div>
         <div className={cssObject.navItem}><NavLink to='/settings'>Settings</NavLink></div>
