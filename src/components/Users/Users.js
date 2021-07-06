cd85912411145279e4b7c4e5b996f7067d486077
Nehorowo <trianglek4nz@gmail.com>
2021-07-06 18:51:06 +0500

    +Axios

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,17 +3,19 @@ import styles from "./Users.module.css";
 import * as axios from "axios";
 
 const Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        debugger;
-        props.setUsers(response.data.items);
-      });
-  }
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
 
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
