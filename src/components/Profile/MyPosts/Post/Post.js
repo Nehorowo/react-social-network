33a48c853c07205387cfd4c12399d875594f27bd
Nehorowo <trianglek4nz@gmail.com>
2021-07-09 15:09:21 +0500

    Users Component refactored into a Class Component, axios request wrapped into componentDidMount

--- src/components/Profile/MyPosts/Post/Post.js
+++ src/components/Profile/MyPosts/Post/Post.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import cssObject from './Post.module.css'
+import React from "react";
+import cssObject from "./Post.module.css";
 
 const Post = (props) => {
-    return (
-      <div className={cssObject.post}>
-        <img src="https://pbs.twimg.com/profile_images/1058731338600247296/OT7thrkK.jpg" />
-        {props.message}
-        <div>
-            <span>Likes: {props.likesCount}</span>
-        </div>
+  return (
+    <div className={cssObject.post}>
+      <img src="https://thispersondoesnotexist.com/image" alt="userPhoto" />
+      {props.message}
+      <div>
+        <span>Likes: {props.likesCount}</span>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Post
+export default Post;
