d8dbe39fa11df9dbb81962c084ba86e90c0f69c4
Nehorowo <trianglek4nz@gmail.com>
2021-07-10 03:39:25 +0500

    Users pagination

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET-USERS";
+const SET_CURRENT_PAGE = "SET-CURRENT-PAGE";
+const SET_TOTAL_USERS_COUNT = "SET-TOTAL-USERS-COUNT";
 
 let initialState = {
   users: [],
+  pageSize: 5,
+  totalUsersCount: 0,
+  currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -29,7 +34,13 @@ const usersReducer = (state = initialState, action) => {
         }),
       };
     case SET_USERS: {
-      return { ...state, users: [...state.users, ...action.users] };
+      return { ...state, users: action.users };
+    }
+    case SET_CURRENT_PAGE: {
+      return { ...state, currentPage: action.currentPage };
+    }
+    case SET_TOTAL_USERS_COUNT: {
+      return { ...state, totalUsersCount: action.totalCount };
     }
 
     default:
@@ -47,4 +58,13 @@ export const unfollowAC = (userId) => ({
 });
 
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({
+  type: SET_CURRENT_PAGE,
+  currentPage,
+});
+export const setTotalUsersCountAC = (totalCount) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  totalCount,
+});
+
 export default usersReducer;
