106bf4a043a1c11a980bc4dc2c050678ed4d7502
Nehorowo <trianglek4nz@gmail.com>
2021-12-03 19:19:02 +0500

    Revert "prepared for deploy, routing at githubpages was fixed by hashrouter"
    This reverts commit ef1d6cb309b8c022123b5875d23c778cd3100e83.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { Suspense } from "react";
+import React from "react";
 import "./App.css";
-import { connect } from "react-redux";
-import { initializeApp } from "./redux/appReducer";
-import { HashRouter, Route, BrowserRouter } from "react-router-dom";
-import Preloader from "./components/common/Preloader/Preloader";
 import Navbar from "./components/Navbar/Navbar";
 import UsersContainer from "./components/Users/UsersContainer";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import { Route, BrowserRouter } from "react-router-dom";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
-
-//lazy loading
-const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
-const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+import { connect } from "react-redux";
+import { initializeApp } from "./redux/appReducer";
+import Preloader from "./components/common/Preloader/Preloader";
 
 class App extends React.Component {
   componentDidMount() {
@@ -23,24 +21,22 @@ class App extends React.Component {
     }
     return (
       <div className='app-wrapper'>
-        <Suspense fallback={<Preloader />}>
-          <HashRouter>
-            <HeaderContainer />
-            <Navbar />
-            <Route path='/dialogs'>
-              <DialogsContainer />
-            </Route>
-            <Route path='/profile/:userId?'>
-              <ProfileContainer />
-            </Route>
-            <Route path='/users'>
-              <UsersContainer />
-            </Route>
-            <Route path='/login'>
-              <LoginPage />
-            </Route>
-          </HashRouter>
-        </Suspense>
+        <BrowserRouter>
+          <HeaderContainer />
+          <Navbar />
+          <Route path='/dialogs'>
+            <DialogsContainer />
+          </Route>
+          <Route path='/profile/:userId?'>
+            <ProfileContainer />
+          </Route>
+          <Route path='/users'>
+            <UsersContainer />
+          </Route>
+          <Route path='/login'>
+            <LoginPage />
+          </Route>
+        </BrowserRouter>
       </div>
     );
   }
