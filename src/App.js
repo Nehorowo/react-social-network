d2fceeab523789b7ead21bfdd6bc26bbab8d962d
Nehorowo <trianglek4nz@gmail.com>
2022-01-10 14:01:53 +0500

    94, react lazy, react suspense

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import UsersContainer from "./components/Users/UsersContainer";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import { Route, BrowserRouter } from "react-router-dom";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginPage from "./components/Login/Login";
-import { connect } from "react-redux";
-import { initializeApp } from "./redux/appReducer";
-import Preloader from "./components/common/Preloader/Preloader";
+import React, { Suspense } from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import UsersContainer from './components/Users/UsersContainer';
+import { Route, BrowserRouter } from 'react-router-dom';
+import HeaderContainer from './components/Header/HeaderContainer';
+import LoginPage from './components/Login/Login';
+import { connect } from 'react-redux';
+import { initializeApp } from './redux/appReducer';
+import Preloader from './components/common/Preloader/Preloader';
+
+//Lazy loading
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
   componentDidMount() {
     this.props.initializeApp();
   }
   render() {
-    if (!this.props.initialized) {
-      return <Preloader />;
-    }
+    // if (!this.props.initialized) {
+    //   return <Preloader />;
+    // }
     return (
-      <div className='app-wrapper'>
+      <div className="app-wrapper">
         <BrowserRouter>
           <HeaderContainer />
           <Navbar />
-          <Route path='/dialogs'>
-            <DialogsContainer />
-          </Route>
-          <Route path='/profile/:userId?'>
-            <ProfileContainer />
-          </Route>
-          <Route path='/users'>
+          <Suspense fallback={<Preloader />}>
+            <Route path="/dialogs">
+              <DialogsContainer />
+            </Route>
+            <Route path="/profile/:userId?">
+              <ProfileContainer />
+            </Route>
+          </Suspense>
+          <Route path="/users">
             <UsersContainer />
           </Route>
-          <Route path='/login'>
+          <Route path="/login">
             <LoginPage />
           </Route>
         </BrowserRouter>
